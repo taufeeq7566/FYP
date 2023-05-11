8518932cc572c29f943be40176f28ff94ae52dc6
taufeeq7566 <lhasnol@gmail.com>
2023-05-12 00:20:14 +0800

    changes on map_screen file

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -13,10 +13,11 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  Completer<GoogleMapController> _controller = Completer();
+  final Completer<GoogleMapController> _controller = Completer();
   late Position _currentPosition;
   final Set<Marker> _markers = {};
   final Geoflutterfire geo = Geoflutterfire();
+  StreamSubscription<List<DocumentSnapshot<Map<String, dynamic>>>>? _subscription;
 
   @override
   void initState() {
@@ -25,15 +26,25 @@ class _MapScreenState extends State<MapScreen> {
     _setGeofence();
   }
 
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+
   void _getCurrentLocation() async {
     final position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
+      desiredAccuracy: LocationAccuracy.high,
+    );
     setState(() {
       _currentPosition = position;
       _markers.add(
         Marker(
-          markerId: MarkerId('current'),
-          position: LatLng(_currentPosition.latitude, _currentPosition.longitude),
+          markerId: const MarkerId('current'),
+          position: LatLng(
+            _currentPosition.latitude,
+            _currentPosition.longitude,
+          ),
           infoWindow: const InfoWindow(title: 'Your Location'),
         ),
       );
@@ -41,70 +52,96 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   Future<void> _setGeofence() async {
-    final checkpoints = await FirebaseFirestore.instance.collection('checkpoints').get();
-    checkpoints.docs.forEach((checkpoint) {
-      final id = checkpoint.id;
-      final data = checkpoint.data();
-      final latitude = data['latitude'];
-      final longitude = data['longitude'];
-      final checkpointRef = FirebaseFirestore.instance.collection('checkpoints').doc(id);
-      final geoPoint = geo.point(latitude: latitude, longitude: longitude);
-      checkpointRef.set({
-        'geofence': geoPoint.data,
-        'isVisited': false,
-      }, SetOptions(merge: true));
+    final checkpointsSnapshot = await FirebaseFirestore.instance
+    .collection('checkpoints')
+    .get();
 
-      final radius = 50.0; // in meters
-      final collectionReference = FirebaseFirestore.instance.collection('geofence');
+for (final checkpointDoc in checkpointsSnapshot.docs) {
+  final id = checkpointDoc.id;
+  final data = checkpointDoc.data();
+  final latitude = data['latitude'];
+  final longitude = data['longitude'];
+  final checkpointRef =
+      FirebaseFirestore.instance.collection('checkpoints').doc(id);
+  final geoPoint = geo.point(latitude: latitude, longitude: longitude);
+  await checkpointRef.set(
+    {
+      'geofence': geoPoint.data,
+      'isVisited': false,
+    },
+    SetOptions(merge: true),
+  );
+      const radius = 50.0; // in meters
+      final collectionReference =
+          FirebaseFirestore.instance.collection('geofence');
       final geoFirePoint = geo.point(latitude: latitude, longitude: longitude);
       final geoFirePointData = {
         'checkpointId': id,
         'isVisited': false,
       };
-      geo.collection(collectionRef: collectionReference).add({
-        'position': geoFirePoint.data,
-        'radius': radius,
-        'data': geoFirePointData,
-      });
-    });
+      geo.collection(collectionRef: collectionReference).add(
+        {
+          'position': geoFirePoint.data,
+          'radius': radius,
+          'data': geoFirePointData,
+        },
+      );
+    }
     _listenForGeofenceEvents();
   }
 
 void _listenForGeofenceEvents() {
   final collectionReference = FirebaseFirestore.instance.collection('geofence');
-  final stream = geo.collection(collectionRef: collectionReference).within(
-    center: geo.point(latitude: _currentPosition.latitude, longitude: _currentPosition.longitude),
-    radius: 50.0, field: '', // in meters
+  final center = geo.point(
+    latitude: _currentPosition.latitude,
+    longitude: _currentPosition.longitude,
   );
-  stream.listen((List<DocumentSnapshot> documentList) {
-    documentList.forEach((DocumentSnapshot document) {
-      final data = document.data() as Map<String, dynamic>; // Cast data to Map<String, dynamic>
-      final checkpointId = data['data']?['checkpointId']; // Add null checks here
-      final isVisited = data['data']?['isVisited']; // Add null checks here
-      if (checkpointId != null && isVisited != null && !isVisited) {
-        final checkpointRef = FirebaseFirestore.instance.collection('checkpoints').doc(checkpointId);
-        checkpointRef.update({'isVisited': true});
+  const radius = 50.0; // in meters
+
+  _subscription = geo
+      .collection(collectionRef: collectionReference)
+      .within(center: center, radius: radius, field: 'position')
+      .listen((List<DocumentSnapshot<Map<String, dynamic>>> documentList) {
+    documentList.forEach((DocumentSnapshot<Map<String, dynamic>> document) {
+      final data = document.data();
+      final checkpointId = data?['data']?['checkpointId'];
+      final isVisited = data?['data']?['isVisited'];
+      if (checkpointId != null && isVisited != null && !isVisited!) {
+        final checkpointRef =
+            FirebaseFirestore.instance.collection('checkpoints').doc(checkpointId);
+        checkpointRef.update({'isVisited': true}).then((_) {
+          print('Checkpoint $checkpointId marked as visited.');
+          // Perform desired actions when a checkpoint is visited
+        }).catchError((error) {
+          print('Failed to mark checkpoint as visited: $error');
+        });
       }
     });
   });
 }
 
- @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text('Map Screen'),
-    ),
-    body: GoogleMap(
-      initialCameraPosition: CameraPosition(
-        target: LatLng(_currentPosition.latitude, _currentPosition.longitude),
-        zoom: 15,
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Map Screen'),
       ),
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-      },
-      markers: _markers,
-    ),
-  );
-}
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            _currentPosition.latitude,
+            _currentPosition.longitude,
+          ),
+          zoom: 16.0,
+        ),
+        markers: _markers,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
+  }
 }
