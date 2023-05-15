7ad35f6d8e6a246e314b347bac27cbcc949e11c7
taufeeq7566 <lhasnol@gmail.com>
2023-05-15 15:58:02 +0800

    modified build.gradle

---
+++ lib/models/permission_handler.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future<bool> requestLocationAndCameraPermissions() async {
+  bool granted = false;
+  
+  final List<Permission> permissions = [
+    Permission.location,
+    Permission.camera,
+  ];
+
+  Map<Permission, PermissionStatus> permissionStatuses = await permissions.request();
+
+  if (permissionStatuses[Permission.location]!.isGranted &&
+      permissionStatuses[Permission.camera]!.isGranted) {
+    granted = true;
+  }
+
+  return granted;
+}
+
+
