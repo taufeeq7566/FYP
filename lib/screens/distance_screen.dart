334714515f7c23b066e6958e1240be65ce6ebadb
taufeeq7566 <lhasnol@gmail.com>
2023-06-11 00:14:04 +0800

    stopwatch feature for distance_screen.dart

--- lib/screens/distance_screen.dart
+++ lib/screens/distance_screen.dart
@@ -4,24 +4,17 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
-
-class Checkpoint {
-  final String name;
-  final double latitude;
-  final double longitude;
-
-  Checkpoint({
-    required this.name,
-    required this.latitude,
-    required this.longitude,
-  });
-  
-    @override
-  String toString() {
-    return 'Checkpoint(name: $name, latitude: $latitude, longitude: $longitude)';
-  }
-}
 class DistanceScreen extends StatefulWidget {
+  final List<DistanceCheckpoint> checkpoints;
+  final String userEmail;
+  final Function(String) onRaceFinished;
+
+  DistanceScreen({
+    required this.checkpoints,
+    required this.userEmail,
+    required this.onRaceFinished,
+  });
+
   @override
   _DistanceScreenState createState() => _DistanceScreenState();
 }
@@ -40,9 +33,12 @@ class _DistanceScreenState extends State<DistanceScreen> {
 
   final DatabaseReference _databaseReference =
       FirebaseDatabase.instance.reference().child('checkpoints');
-  List<Checkpoint> checkpoints = [];
+  List<DistanceCheckpoint> checkpoints = [];
   List<double> distances = [];
-  late Timer _timer;
+  Timer? _timer;
+  late Stopwatch _stopwatch;
+  bool _isRaceFinished = false;
+  String _userFullName = '';
 
   @override
   void initState() {
@@ -50,11 +46,12 @@ class _DistanceScreenState extends State<DistanceScreen> {
     _getCurrentLocation();
     _startDistanceUpdates();
     _retrieveCheckpoints();
+    _stopwatch = Stopwatch();
+    _retrieveFullName();
   }
 
   @override
   void dispose() {
-    _stopDistanceUpdates();
     super.dispose();
   }
 
@@ -62,20 +59,23 @@ class _DistanceScreenState extends State<DistanceScreen> {
     final position = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high,
     );
-    setState(() {
-      _currentPosition = position;
-    });
-  }
+      setState(() {
+        _currentPosition = position;
+  });
+    }
+  
 
   void _startDistanceUpdates() {
-    _timer = Timer.periodic(const Duration(seconds: 2), (_) {
-      _getCurrentLocation();
-      _calculateDistance();
-    });
+    if (_timer == null || !_timer!.isActive) {
+      _timer = Timer.periodic(const Duration(milliseconds: 1), (_) {
+        _getCurrentLocation();
+        _calculateDistance();
+      });
+    }
   }
 
   void _stopDistanceUpdates() {
-    _timer.cancel();
+    _timer?.cancel();
   }
 
   void _retrieveCheckpoints() {
@@ -83,25 +83,25 @@ class _DistanceScreenState extends State<DistanceScreen> {
       final snapshot = event.snapshot;
       if (snapshot.value != null) {
         Map<dynamic, dynamic> data = snapshot.value as Map<dynamic, dynamic>;
-        List<Checkpoint> retrievedCheckpoints = [];
+        List<DistanceCheckpoint> retrievedCheckpoints = [];
 
         data.forEach((key, value) {
           String name = value['name'];
           double latitude = value['latitude'];
           double longitude = value['longitude'];
 
-          retrievedCheckpoints.add(Checkpoint(
+          retrievedCheckpoints.add(DistanceCheckpoint(
             name: name,
             latitude: latitude,
             longitude: longitude,
           ));
         });
 
+        retrievedCheckpoints.sort((a, b) => a.name.compareTo(b.name));
+
         setState(() {
           checkpoints = retrievedCheckpoints;
         });
-
-        print('Checkpoints: $checkpoints'); // Print the list of checkpoints
       }
     }).catchError((error) {
       print("Failed to fetch checkpoints: $error");
@@ -109,9 +109,10 @@ class _DistanceScreenState extends State<DistanceScreen> {
   }
 
   void _calculateDistance() {
-    List<double> updatedDistances = []; // Create a new list to store the updated distances
+    List<double> updatedDistances = List<double>.filled(checkpoints.length, 0.0);
 
-    checkpoints.forEach((checkpoint) {
+    for (int i = 0; i < checkpoints.length; i++) {
+      DistanceCheckpoint checkpoint = checkpoints[i];
       double distance = Geolocator.distanceBetween(
         _currentPosition.latitude,
         _currentPosition.longitude,
@@ -119,75 +120,185 @@ class _DistanceScreenState extends State<DistanceScreen> {
         checkpoint.longitude,
       );
 
-      String formattedDistance =
-          distance.toStringAsFixed(2); // Format distance to two decimal places
-      updatedDistances.add(distance); // Add the distance to the new list
+      String formattedDistance = distance.toStringAsFixed(2);
+      updatedDistances[i] = distance;
+
       print('Distance to ${checkpoint.name}: $formattedDistance meters');
 
-      // Check if the user is within the geofence radius (e.g., 20 meters)
-      if (distance <= 20) {
-        // User is within the geofence, perform desired actions (e.g., show a notification)
+      if (distance <= 7 && !_isRaceFinished) {
         print('User is within the geofence of ${checkpoint.name}');
+
+        if (checkpoint.name == 'Starting Line') {
+          _startRace();
+        } else if (checkpoint.name == 'Finish Line') {
+          _stopRace();
+          _uploadLeaderboard();
+        } else {
+          setState(() {
+            // Update the stopwatch time for the current checkpoint
+            checkpoint.stopwatchTime = _stopwatch.elapsed;
+          });
+        }
       }
-    });
+    }
 
     setState(() {
-      distances = updatedDistances; // Update the distances list with the new distances
+      distances = updatedDistances;
     });
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text('Distance Screen'),
-    ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Current Location:',
-            style: TextStyle(fontSize: 18),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Latitude: ${_currentPosition.latitude}',
-            style: TextStyle(fontSize: 16),
-          ),
-          Text(
-            'Longitude: ${_currentPosition.longitude}',
-            style: TextStyle(fontSize: 16),
-          ),
-          SizedBox(height: 20),
-          if (checkpoints.isEmpty)
+void _uploadLeaderboard() {
+  DatabaseReference leaderboardRef =
+      FirebaseDatabase.instance.reference().child('leaderboard');
+
+  for (int i = 0; i < checkpoints.length; i++) {
+    DistanceCheckpoint checkpoint = checkpoints[i];
+    Duration? stopwatchTime = checkpoint.stopwatchTime;
+
+    if (stopwatchTime != null) {
+      String formattedStopwatchTime = _formatDuration(stopwatchTime);
+      String checkpointKey = 'checkpoint${i + 1}';
+
+      Map<String, String> leaderboardData = {
+        checkpointKey: formattedStopwatchTime,
+      };
+
+      leaderboardRef.child(_userFullName).update(leaderboardData).then((_) {
+        print('Leaderboard data uploaded for $checkpointKey');
+      }).catchError((error) {
+        print('Failed to upload leaderboard data for $checkpointKey: $error');
+      });
+    }
+  }
+}
+
+
+
+  void _startRace() {
+    setState(() {
+      _isRaceFinished = false;
+      _stopwatch.start();
+    });
+  }
+
+  void _stopRace() {
+    widget.onRaceFinished(widget.userEmail);
+    setState(() {
+      _isRaceFinished = true;
+      _stopwatch.stop();
+    });
+  }
+
+  void _retrieveFullName() {
+    DatabaseReference userRef =
+        FirebaseDatabase.instance.reference().child('users');
+
+    userRef.child(widget.userEmail).once().then((event) {
+      final snapshot = event.snapshot;
+      if (snapshot.value != null) {
+        String? fullName = snapshot.value as String?;
+        if (fullName != null) {
+          setState(() {
+            _userFullName = fullName;
+          });
+        }
+      }
+    }).catchError((error) {
+      print("Failed to fetch user's full name: $error");
+    });
+  }
+
+  String _formatDuration(Duration duration) {
+    String minutes =
+        duration.inMinutes.remainder(60).toString().padLeft(2, '0');
+    String seconds =
+        duration.inSeconds.remainder(60).toString().padLeft(2, '0');
+    String milliseconds =
+        (duration.inMilliseconds.remainder(1000) ~/ 10).toString().padLeft(2, '0');
+
+    return '$minutes:$seconds:$milliseconds';
+  }
+
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Distance Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
             Text(
-              'No Checkpoints Available',
+              'Current Location:',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Latitude: ${_currentPosition.latitude}',
               style: TextStyle(fontSize: 16),
-            )
-          else
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: checkpoints.length,
-              itemBuilder: (context, index) {
-                final distance = distances.length > index ? distances[index].toStringAsFixed(2) : 'N/A';
-                return ListTile(
-                  title: Text(
-                    checkpoints[index].name,
+            ),
+            Text(
+              'Longitude: ${_currentPosition.longitude}',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+            if (_isRaceFinished)
+              Text(
+                'Race Finished',
+                style: TextStyle(fontSize: 18),
+              )
+            else
+              Column(
+                children: [
+                  Text(
+                    'Race in progress',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Time: ${_formatDuration(_stopwatch.elapsed)}',
                     style: TextStyle(fontSize: 16),
                   ),
-                  subtitle: Text(
-                    'Distance: $distance meters',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                );
-              },
+                ],
+              ),
+            SizedBox(height: 20),
+            Text(
+              'Distances to Checkpoints:',
+              style: TextStyle(fontSize: 18),
             ),
-        ],
+            SizedBox(height: 10),
+            for (int i = 0; i < checkpoints.length; i++)
+              Column(
+                children: [
+                  Text(
+                    '${checkpoints[i].name}: ${distances[i].toStringAsFixed(2)} meters',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  if (checkpoints[i].stopwatchTime != null)
+                    Text(
+                      'Time: ${_formatDuration(checkpoints[i].stopwatchTime!)}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                ],
+              ),
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
 
+class DistanceCheckpoint {
+  final String name;
+  final double latitude;
+  final double longitude;
+  Duration? stopwatchTime;
 
+  DistanceCheckpoint({
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+    this.stopwatchTime,
+  });
+}
