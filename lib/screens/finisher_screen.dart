230b8391bdb90877e1f8479710486d91acbe0b4e
taufeeq7566 <lhasnol@gmail.com>
2023-06-14 17:29:40 +0800

    Leaderboard and Finisher page are completed

--- lib/screens/finisher_screen.dart
+++ lib/screens/finisher_screen.dart
@@ -1,19 +1,85 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-
 
 class FinisherScreen extends StatefulWidget {
   @override
-   _FinisherScreenState createState() => _FinisherScreenState();
+  _FinisherScreenState createState() => _FinisherScreenState();
 }
 
 class _FinisherScreenState extends State<FinisherScreen> {
-  
+  final DatabaseReference _databaseReference =
+      FirebaseDatabase.instance.reference();
+
+  List<String> _finishers = [];
+
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  void initState() {
+    super.initState();
+    _fetchFinishers();
   }
+
+  Future<void> _fetchFinishers() async {
+    DatabaseReference checkpointsRef = _databaseReference.child('checkpoints');
+    DataSnapshot checkpointSnapshot =
+        (await checkpointsRef.once()).snapshot;
+
+    if (checkpointSnapshot.value == null) {
+      return;
+    }
+
+    Map<dynamic, dynamic> checkpoints =
+        checkpointSnapshot.value as Map<dynamic, dynamic>;
+    int numCheckpoints = checkpoints.length;
+
+    DatabaseReference leaderboardRef = _databaseReference.child('leaderboard');
+    DataSnapshot leaderboardSnapshot =
+        (await leaderboardRef.once()).snapshot;
+
+    if (leaderboardSnapshot.value == null) {
+      return;
+    }
+
+    Map<dynamic, dynamic> leaderboardData =
+        leaderboardSnapshot.value as Map<dynamic, dynamic>;
+
+    leaderboardData.forEach((key, value) {
+      String fullName = key;
+      Map<dynamic, dynamic> userCheckpoints = value['checkpoints'];
+
+      if (userCheckpoints.length == numCheckpoints) {
+        _finishers.add(fullName);
+      }
+    });
+
+    setState(() {
+      // Update the state to trigger a rebuild with the fetched finishers
+      _finishers = _finishers;
+    });
+  }
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Finishers'),
+    ),
+    body: ListView.builder(
+      itemCount: _finishers.length,
+      itemBuilder: (context, index) {
+        String fullName = _finishers[index];
+        return ListTile(
+          title: Text(
+            fullName,
+            style: TextStyle(
+              fontSize: 20, // Adjust the font size as desired
+              fontWeight: FontWeight.bold, // Make the font bold
+            ),
+          ),
+          leading: Icon(Icons.emoji_events), // Add the trophy icon
+        );
+      },
+    ),
+  );
+}
+
 }
--- lib/screens/leaderboard_screen.dart
+++ lib/screens/leaderboard_screen.dart
@@ -1,18 +1,166 @@
+import 'dart:async';
+
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 class LeaderboardScreen extends StatefulWidget {
   @override
-   _LeaderboardScreenState createState() => _LeaderboardScreenState();
+  _LeaderboardScreenState createState() => _LeaderboardScreenState();
 }
 
 class _LeaderboardScreenState extends State<LeaderboardScreen> {
-  
+  final DatabaseReference _databaseReference =
+      FirebaseDatabase.instance.reference().child('leaderboard');
+
+  List<LeaderboardEntry> _leaderboardData = [];
+
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  void initState() {
+    super.initState();
+    _fetchLeaderboardData();
+  }
+
+Future <void> _fetchLeaderboardData() async {
+  _databaseReference.onValue.listen((event) {
+    final data = event.snapshot.value as Map<dynamic, dynamic>;
+    List<LeaderboardEntry> leaderboard = [];
+    data.forEach((key, value) {
+      String fullName = key;
+      Map<dynamic, dynamic> checkpoints = value['checkpoints'];
+
+      List<CheckpointEntry> checkpointEntries = [];
+      String? finishLineTime;
+
+checkpoints.forEach((checkpoint, checkpointTime) {
+  if (checkpointTime == '00:00:00') {
+    checkpointEntries.insert(
+      0,
+      CheckpointEntry(
+        checkpoint: 'Starting Line',
+        checkpointTime: checkpointTime,
+      ),
+    );
+  } else if (checkpointTime == checkpoints.values.last) {
+    finishLineTime = checkpointTime;
+  } else {
+    String checkpointName = 'Checkpoint ${checkpointEntries.length + 1}';
+    checkpointEntries.add(
+      CheckpointEntry(
+        checkpoint: checkpointName,
+        checkpointTime: checkpointTime,
+      ),
+    );
+  }
+});
+
+
+      // Sort the checkpoint entries based on their names
+      checkpointEntries.sort((a, b) => a.checkpoint.compareTo(b.checkpoint));
+
+      if (finishLineTime != null) {
+        checkpointEntries.add(
+          CheckpointEntry(
+            checkpoint: 'Finish Line',
+            checkpointTime: finishLineTime!,
+          ),
+        );
+      }
+
+      leaderboard.add(
+        LeaderboardEntry(
+          fullName: fullName,
+          checkpointEntries: checkpointEntries,
+        ),
+      );
+    });
+
+leaderboard.sort((a, b) {
+  // If the first entry has the "Starting Line" checkpoint, it should be first
+  if (a.checkpointEntries[0].checkpoint == 'Starting Line') {
+    // If the second entry also has the "Starting Line" checkpoint, compare their last checkpoint times
+    if (b.checkpointEntries[0].checkpoint == 'Starting Line') {
+      return a.lastCheckpointTime.compareTo(b.lastCheckpointTime);
+    }
+    return -1; // First entry has the "Starting Line" checkpoint, so it should be first
+  }
+  // If the second entry has the "Starting Line" checkpoint, it should be first
+  else if (b.checkpointEntries[0].checkpoint == 'Starting Line') {
+    return 1;
+  }
+  // Sort based on the number of checkpoint entries
+  return a.checkpointEntries.length.compareTo(b.checkpointEntries.length);
+});
+
+
+
+    setState(() {
+      _leaderboardData = leaderboard;
+    });
+  });
+}
+
+
+@override
+void dispose() {
+  super.dispose();
+}
+
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Leaderboard'),
+    ),
+    body: RefreshIndicator(
+      onRefresh: () async {
+        await _fetchLeaderboardData();
+      },
+      child: ListView.builder(
+        itemCount: _leaderboardData.length,
+        itemBuilder: (context, index) {
+          LeaderboardEntry entry = _leaderboardData[index];
+          return ListTile(
+            title: Text(entry.fullName),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: entry.checkpointEntries.map((checkpointEntry) {
+                return Text('${checkpointEntry.checkpoint}: ${checkpointEntry.checkpointTime}');
+              }).toList(),
+            ),
+            trailing: Text('Rank: ${index + 1}'),
+          );
+        },
+      ),
+    ),
+  );
+}
+
+
+
+}
+
+class LeaderboardEntry {
+  final String fullName;
+  final List<CheckpointEntry> checkpointEntries;
+
+  LeaderboardEntry({
+    required this.fullName,
+    List<CheckpointEntry>? checkpointEntries,
+  }) : checkpointEntries = checkpointEntries ?? [];
+
+  String get lastCheckpointTime {
+    if (checkpointEntries.isNotEmpty) {
+      return checkpointEntries.last.checkpointTime;
+    }
+    return '';
   }
 }
+
+class CheckpointEntry {
+  final String checkpoint;
+  final String checkpointTime;
+
+  CheckpointEntry({required this.checkpoint, required this.checkpointTime});
+}
+
